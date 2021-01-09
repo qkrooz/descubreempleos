d357dd7efd9b5b30b66fe99b154cca6cb117c3dd
diego41071 <alexander.diego.2007@hotmail.com>
2021-01-09 16:48:32 -0500

    responsive en login

--- pages/login/style.js
+++ pages/login/style.js
@@ -4,6 +4,7 @@ export const LoginStyle = styled.div`
   background-color: #cf0022;
   text-align: center;
   height: 100vh;
+  width: 100%;
   img {
     width: 500px;
     margin-bottom: -105px;
@@ -12,7 +13,7 @@ export const LoginStyle = styled.div`
 
   form {
     margin: 0 auto;
-    width: 32%;
+    width: 422px;
     background-color: white;
     padding: 3%;
     border-radius: 10px;
@@ -36,5 +37,19 @@ export const LoginStyle = styled.div`
     margin-top: 31px;
     font-size: 18px;
     font-weight: bold;
+  }
+  @media screen and (max-width: 600px) {
+    & form {
+      width: 300px;
+    }
+    & img {
+      width: 317px;
+      margin-top: -54px;
+      margin-bottom: -63px;
+    }
+    & .textwhite {
+      margin-top: 122px;
+      font-size: 13px;
+    }
   }
 `;
