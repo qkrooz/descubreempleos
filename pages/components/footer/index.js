e2b953fbaa2ca9781af05aeb731e419fcaaae2b8
diego41071 <alexander.diego.2007@hotmail.com>
2021-01-20 11:56:57 -0500

    cambios en footer

--- pages/components/footer/index.js
+++ pages/components/footer/index.js
@@ -1,14 +1,30 @@
 import React from "react";
-import { Icon } from "semantic-ui-react";
+import { Grid, Icon } from "semantic-ui-react";
 import footer from "../../../styles/footer.module.css";
 
 export const FooterContainer = () => {
   return (
     <div>
       <footer className={footer.container}>
-        <Icon className="copyright outline"></Icon> Acerca de nosotros |Términos
-        y condiciones| Politica de privacidad| Contáctanos Todos los derechos
-        reservados - Descubre Sa. de CV.
+        <Grid columns={2} stackable>
+          <Grid.Column>
+            <h3>
+              Acerca de nosotros | Términos y condiciones | Politica de
+              privacidad | Contáctanos
+            </h3>
+            <h4>
+              <Icon className="copyright outline"></Icon>Todos los derechos
+              reservados - Descubre Sa. de CV.
+            </h4>
+          </Grid.Column>
+          <Grid.Column>
+            <div className={footer.floatimg}>
+              <img src="/icon-facebook-blue.png" className={footer.icon}></img>
+              <img src="/icon-youtube-white.png" className={footer.icon}></img>
+              <img src="/icon-twitter-blue.png" className={footer.icon}></img>
+            </div>
+          </Grid.Column>
+        </Grid>
       </footer>
     </div>
   );
