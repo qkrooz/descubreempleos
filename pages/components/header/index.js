c19600eaca500471587109f3d7d886454c32f66a
diego41071 <alexander.diego.2007@hotmail.com>
2021-01-20 14:55:25 -0500

    ajustes en header

--- pages/components/header/index.js
+++ pages/components/header/index.js
@@ -1,22 +1,34 @@
 import React from "react";
+import { Grid } from "semantic-ui-react";
 import header from "../../../styles/header.module.css";
 
 export const HeaderComponent = () => {
   return (
     <div>
       <header className={header.container}>
-        <img src="/icon-white2.png" className={header.logo}></img>
-        <div className={header.navContainer}>
-          <img src="/icon-home.png" className={header.icon}></img>
-          <a className={header.link}>Inicio</a>
-          <img src="/icon-search.png" className={header.icon}></img>
-          <a className={header.link}>Buscar</a>
-          <img src="/icon-info.png" className={header.icon}></img>
-          <a className={header.link}>Usuario</a>
-        </div>
-        <img src="/icon-message.png" className={header.icon}></img>
-        <img src="/icon-lamp-white.png" className={header.icon}></img>
-        <img src="/icon-list.png" className={header.icon}></img>
+        <Grid columns={3} stackable>
+          <Grid.Column>
+            <img src="/icon-white2.png" className={header.logo}></img>
+          </Grid.Column>
+          <Grid.Column>
+            <div className={header.navContainer}>
+              <img src="/icon-home.png" className={header.icon}></img>
+              <a className={header.link}>Inicio</a>
+              <img src="/icon-search.png" className={header.icon}></img>
+              <a className={header.link}>Buscar</a>
+              <img src="/icon-info.png" className={header.icon}></img>
+              <a className={header.link}>Usuario</a>
+            </div>
+          </Grid.Column>
+          <Grid.Column>
+            <div className={header.containericons}>
+              <img src="/icon-message.png" className={header.icon}></img>
+              <img src="/icon-lamp-white.png" className={header.icon}></img>
+              <img src="/icon-list.png" className={header.icon}></img>
+            </div>
+          </Grid.Column>
+        </Grid>
+
         <div className={header.controlsContainer}></div>
       </header>
     </div>
