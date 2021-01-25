b3639b95169245b8775385db8ad4fa5d0f8692d7
diego41071 <alexander.diego.2007@hotmail.com>
2021-01-24 21:06:23 -0500

    cambios en busqueda

--- pages/busqueda/index.js
+++ pages/busqueda/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Grid } from "semantic-ui-react";
+import { Button, Card, Grid, Input } from "semantic-ui-react";
 import style from "../../styles/busqueda.module.css";
 
 export const BusquedaComponent = () => {
@@ -9,10 +9,9 @@ export const BusquedaComponent = () => {
         <h4 className={style.title}>
           Encuentra ese trabajo que has estado esperando
         </h4>
-        ¿Qué estas buscando?
-        <input placeholder="Puesto, profeción o campo"></input>
-        ¿Donde estas buscando?<input placeholder="Municipio, Estado"></input>
-        <button>Filtros</button> <button>Buscar</button>
+        <Grid columns={3} stackable> <Grid.Column><label>¿Qué estas buscando?</label><br></br>
+          <Input icon='clock outline' iconPosition='left' placeholder="Puesto, profeción o campo"></Input></Grid.Column><Grid.Column><label>  ¿Donde estas buscando?</label><br></br><Input icon='map marker alternate' iconPosition='left' placeholder="Municipio, Estado"></Input></Grid.Column><Grid.Column>        <Button>Filtros</Button> <Button color="yellow">Buscar</Button>
+          </Grid.Column></Grid>
       </div>
       <Grid columns={2} stackable>
         <Grid.Column>
@@ -51,7 +50,7 @@ export const BusquedaComponent = () => {
                   <Grid.Column width={3}>
                     <img
                       className={style.image}
-                      src="https://ingenieria.udd.cl/files/2020/09/walmart-logo-1-1.jpg"
+                      src="https://tse1.mm.bing.net/th?id=OIP.CoDDxOo6rl3wxDgGzUmjBgHaGK&pid=Api&P=0&w=202&h=169"
                     ></img>
                   </Grid.Column>
                   <Grid.Column>
@@ -73,17 +72,17 @@ export const BusquedaComponent = () => {
           </Card>
         </Grid.Column>
         <Grid.Column>
-          <Card className={style.card}>
+          <Card className={style.card1}>
             <Card.Content>
               <Card.Description>
-                Activar alerta por correo con estos parametros de busqueda
-                Activar
+                <h3>Activar alerta por correo con estos parametros de busqueda</h3>
+                <Button color="yellow">Activar</Button>
               </Card.Description>
             </Card.Content>
           </Card>
-          <Card className={style.card}>
+          <Card className={style.card1}>
             <Card.Content>
-              <Card.Description>Postulaciónes activas 33</Card.Description>
+              <Card.Description><h3>Postulaciónes activas</h3> <h1 className={style.h1}>33</h1></Card.Description>
             </Card.Content>
           </Card>
         </Grid.Column>
