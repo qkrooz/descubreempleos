d46c67a8e6da81c5b84993b20a2ae59e36741ef4
qkrooz <46641568+qkrooz@users.noreply.github.com>
2021-03-15 15:30:29 -0600

    .datos
    Terminado el formulario y modal, falta la sumission del formuilario que aun no se puede hacer desde el boton

--- pages/_api/components/UserCard.js
+++ pages/_api/components/UserCard.js
@@ -4,10 +4,22 @@ import apiRoute from "../resources/apiRoute";
 import { Card, Checkbox, Icon } from "semantic-ui-react";
 import style from "../../../styles/usercard.module.css";
 import axios from "axios";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+} from "@chakra-ui/react";
+import { Field, Form, Formik } from "formik";
 const UserCard = React.memo(() => {
   // states
   const [userImgError, setUserImgError] = useState(false);
   const [disponibleState, setDisponibleState] = useState();
+  const [editModalVisibility, setEditModalVisibility] = useState(false);
   // context
   const { userInfoState, secondaryInfoState } = useContext(MainContext);
   const [userInfo] = userInfoState;
@@ -37,46 +49,182 @@ const UserCard = React.memo(() => {
     console.log(secondaryInfo);
   }, [secondaryInfo]);
   return (
-    <Card className={style.profileContainer}>
-      <Icon className={style.edit} size="large" name="edit" />
-      {!userImgError ? (
-        <img
-          className={style.profile}
-          src={`${apiRoute}/img/user-profile/${userInfo.ID}.jpg`}
-          onError={() => {
-            setUserImgError(true);
+    <>
+      <Card className={style.profileContainer}>
+        <button
+          className={style.edit}
+          onClick={() => {
+            setEditModalVisibility(true);
           }}
-        />
-      ) : (
-        <div className={style.iconContainer}>
-          <Icon name="user" size="huge" color="grey" />
-        </div>
-      )}
-      <h3
-        className={style.h3}
-        style={{ textTransform: "capitalize" }}
-      >{`${userInfo.NAMES} ${userInfo.LAST_NAME} ${userInfo.MOTHERS_LAST_NAME}`}</h3>
-      {userInfo.TITLE ? (
-        <h3 className={style.h3}>{userInfo.TITLE}</h3>
-      ) : (
+        >
+          <Icon size="large" name="edit" />
+        </button>
+        {!userImgError ? (
+          <img
+            className={style.profile}
+            src={`${apiRoute}/img/user-profile/${userInfo.ID}.jpg`}
+            onError={() => {
+              setUserImgError(true);
+            }}
+          />
+        ) : (
+          <div className={style.iconContainer}>
+            <Icon name="user" size="huge" color="grey" />
+          </div>
+        )}
         <h3
           className={style.h3}
-          style={{ color: "gray", fontWeight: "normal" }}
-        >
-          Titulo no disponible
-        </h3>
-      )}
+          style={{ textTransform: "capitalize" }}
+        >{`${userInfo.NAMES} ${userInfo.LAST_NAME} ${userInfo.MOTHERS_LAST_NAME}`}</h3>
+        {userInfo.TITLE ? (
+          <h3 className={style.h3}>{userInfo.TITLE}</h3>
+        ) : (
+          <h3
+            className={style.h3}
+            style={{ color: "gray", fontWeight: "normal" }}
+          >
+            Titulo no disponible
+          </h3>
+        )}
 
-      <a className={style.linkcv}> CV.PDF</a>
-      <div className={style.flextoggle}>
-        <h3 className={style.yellowtext}>Disponible para trabajar</h3>
-        <Checkbox
-          toggle
-          defaultChecked={Boolean(parseInt(secondaryInfo.DISPONIBLE))}
-          onChange={ChangeAvailability}
-        />
-      </div>
-    </Card>
+        <a className={style.linkcv}> CV.PDF</a>
+        <div className={style.flextoggle}>
+          <h3 className={style.yellowtext}>Disponible para trabajar</h3>
+          <Checkbox
+            toggle
+            defaultChecked={Boolean(parseInt(secondaryInfo.DISPONIBLE))}
+            onChange={ChangeAvailability}
+          />
+        </div>
+      </Card>
+      <Formik
+        initialValues={{
+          NAMES:
+            userInfo.NAMES.charAt(0).toUpperCase() + userInfo.NAMES.slice(1),
+          LAST_NAME:
+            userInfo.LAST_NAME.charAt(0).toUpperCase() +
+            userInfo.LAST_NAME.slice(1),
+          MOTHERS_LAST_NAME:
+            userInfo.MOTHERS_LAST_NAME.charAt(0).toUpperCase() +
+            userInfo.MOTHERS_LAST_NAME.slice(1),
+          TITULO: secondaryInfo.TITULO
+            ? secondaryInfo.TITULO.charAt(0).toUpperCase() +
+              secondaryInfo.TITULO.slice(1)
+            : "",
+        }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ values, handleChange, errors, handleBlur }) => (
+          <>
+            <Modal
+              isOpen={editModalVisibility}
+              onClose={() => {
+                setEditModalVisibility(!editModalVisibility);
+              }}
+              size="xl"
+            >
+              <ModalOverlay />
+              <ModalContent>
+                <ModalHeader>
+                  ¿Quieres actualizar tus datos personales?
+                </ModalHeader>
+                <ModalCloseButton />
+                <ModalBody
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    width: "100%",
+                  }}
+                >
+                  <div className={style.modal1Upper}>
+                    {!userImgError ? (
+                      <button>
+                        <img
+                          className={style.profile}
+                          src={`${apiRoute}/img/user-profile/${userInfo.ID}.jpg`}
+                          onError={() => {
+                            setUserImgError(true);
+                          }}
+                        />
+                      </button>
+                    ) : (
+                      <div className={style.iconContainer}>
+                        <Icon name="user" size="huge" color="grey" />
+                      </div>
+                    )}
+                    <Form style={{ display: "flex", flexDirection: "column" }}>
+                      <Field
+                        type="text"
+                        placeholder="Nombres"
+                        name="NAMES"
+                        onChange={handleChange}
+                        value={values.NAMES}
+                        onBlur={handleBlur}
+                      />
+                      <Field
+                        type="text"
+                        placeholder="Apellido"
+                        name="LAST_NAME"
+                        onChange={handleChange}
+                        value={values.LAST_NAME}
+                        onBlur={handleBlur}
+                      />
+                      <Field
+                        type="text"
+                        placeholder="Apellido Materno"
+                        name="MOTHERS_LAST_NAME"
+                        onChange={handleChange}
+                        value={values.MOTHERS_LAST_NAME}
+                        onBlur={handleBlur}
+                      />
+                      <Field
+                        type="text"
+                        placeholder="Titulo/Puesto"
+                        name="TITULO"
+                        onChange={handleChange}
+                        value={values.TITULO}
+                        onBlur={handleBlur}
+                      />
+                    </Form>
+                  </div>
+                  <div className={style.modal1Lower}>
+                    <div>
+                      <p href="#">Fotografía</p>
+                      <p>
+                        Puedes escoger la fotografía que prefieras pero por
+                        motivos profecionales sugerimos una fotografía de
+                        aspecto formal y seria.
+                      </p>
+                    </div>
+                    <div>
+                      <a href="#">Generar y actualizar CV</a>
+                      <p>
+                        Crea un currículum con nosotros usando tus datos en esta
+                        sección para generarlo de forma automatica y así puedas
+                        enviarlo en tus postulaciones de empleo.
+                      </p>
+                    </div>
+                  </div>
+                </ModalBody>
+                <ModalFooter style={{ justifyContent: "center" }}>
+                  <Button
+                    variant="ghost"
+                    onClick={() => {
+                      setEditModalVisibility(!editModalVisibility);
+                    }}
+                  >
+                    Cancelar
+                  </Button>
+                  <Button type="submit" style={{ backgroundColor: "#ECB83C" }}>
+                    Actualizar
+                  </Button>
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
+          </>
+        )}
+      </Formik>
+    </>
   );
 });
 export default UserCard;
--- pages/_api/screens/Datos.js
+++ pages/_api/screens/Datos.js
@@ -1,68 +1,317 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { MainContext } from "../resources/MainContext";
-import { Card, Grid } from "semantic-ui-react";
+import { Card, Icon } from "semantic-ui-react";
 import style from "../../../styles/datos.module.css";
-import { PlusCircleOutlined } from "@ant-design/icons";
 // components
 import Footer from "../components/Footer";
 import UserCard from "../components/UserCard";
 export default function DatoComponent() {
   // state
   // context
-  const {} = useContext(MainContext);
+  const { userInfoState, secondaryInfoState } = useContext(MainContext);
+  const [userInfo] = userInfoState;
+  const [secondaryInfo] = secondaryInfoState;
+  const [modalsVisibility, setModalsVisibility] = useState({
+    modal1: false,
+    modal2: false,
+    modal3: false,
+  });
   return (
     <>
       <div className={style.container}>
         <div className={style.left}>
           <UserCard />
+          <Card style={{ padding: "1em" }}>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <span style={{ fontWeight: "bold", fontSize: "1.3em" }}>
+                Datos personales
+              </span>
+              <button
+                className={style.modalButton}
+                onClick={() => {
+                  setModalsVisibility({ ...modalsVisibility, modal1: true });
+                }}
+              >
+                <Icon name="edit" size="large" />
+              </button>
+            </div>
+            <div>
+              <table style={{ width: "100%" }}>
+                <tbody className={style.personalTbody}>
+                  <tr>
+                    <td className={style.personalLabel}>Edad</td>
+                    <td>
+                      <div
+                        style={{
+                          width: "3em",
+                          borderBottom: "2px solid gray",
+                          margin: "0 1em",
+                        }}
+                      />
+                    </td>
+                    <td>
+                      {userInfo.AGE ? (
+                        userInfo.AGE
+                      ) : (
+                        <div>
+                          <Icon name="warning circle" />
+                          <span>No disponible</span>
+                        </div>
+                      )}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className={style.personalLabel}>Género</td>
+                    <td>
+                      <div
+                        style={{
+                          width: "3em",
+                          borderBottom: "2px solid gray",
+                          margin: "0 1em",
+                        }}
+                      />
+                    </td>
+                    <td>
+                      {userInfo.GENRE ? (
+                        userInfo.GENRE
+                      ) : (
+                        <div>
+                          <Icon name="warning circle" />
+                          <span>No disponible</span>
+                        </div>
+                      )}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className={style.personalLabel}>Teléfono</td>
+                    <td>
+                      <div
+                        style={{
+                          width: "3em",
+                          borderBottom: "2px solid gray",
+                          margin: "0 1em",
+                        }}
+                      />
+                    </td>
+                    <td>
+                      {userInfo.TEL_NUMBER ? (
+                        userInfo.TEL_NUMBER
+                      ) : (
+                        <div>
+                          <Icon name="warning circle" />
+                          <span>No disponible</span>
+                        </div>
+                      )}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className={style.personalLabel}>Estado</td>
+                    <td>
+                      <div
+                        style={{
+                          width: "3em",
+                          borderBottom: "2px solid gray",
+                          margin: "0 1em",
+                        }}
+                      />
+                    </td>
+                    <td>
+                      {userInfo.STATE ? (
+                        userInfo.STATE
+                      ) : (
+                        <div>
+                          <Icon name="warning circle" />
+                          <span>No disponible</span>
+                        </div>
+                      )}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className={style.personalLabel}>Ciudad</td>
+                    <td>
+                      <div
+                        style={{
+                          width: "3em",
+                          borderBottom: "2px solid gray",
+                          margin: "0 1em",
+                        }}
+                      />
+                    </td>
+                    <td>
+                      {userInfo.CITY ? (
+                        userInfo.CITY
+                      ) : (
+                        <div>
+                          <Icon name="warning circle" />
+                          <span>No disponible</span>
+                        </div>
+                      )}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className={style.personalLabel}>Idiomas</td>
+                    <td>
+                      <div
+                        style={{
+                          width: "3em",
+                          borderBottom: "2px solid gray",
+                          margin: "0 1em",
+                        }}
+                      />
+                    </td>
+                    <td>
+                      {secondaryInfo.LANGUAGES ? (
+                        secondaryInfo.LANGUAGES
+                      ) : (
+                        <div>
+                          <Icon name="warning circle" />
+                          <span>No disponible</span>
+                        </div>
+                      )}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </Card>
+          <Card style={{ padding: "1em" }}>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <span style={{ fontWeight: "bold", fontSize: "1.3em" }}>
+                Habilidades
+              </span>
+              <button
+                className={style.modalButton}
+                onClick={() => {
+                  setModalsVisibility({ ...modalsVisibility, modal2: true });
+                }}
+              >
+                <Icon name="edit" size="large" />
+              </button>
+            </div>
+            <div>
+              {secondaryInfo.HABILIDADES ? (
+                secondaryInfo.HABILIDADES
+              ) : (
+                <div>
+                  <Icon name="warning circle" />
+                  <span>No disponible</span>
+                </div>
+              )}
+            </div>
+          </Card>
+          <Card style={{ padding: "1em" }}>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <span style={{ fontWeight: "bold", fontSize: "1.3em" }}>
+                Seguridad
+              </span>
+              <button
+                className={style.modalButton}
+                onClick={() => {
+                  setModalsVisibility({ ...modalsVisibility, modal3: true });
+                }}
+              >
+                <Icon name="edit" size="large" />
+              </button>
+            </div>
+            <div>
+              <table style={{ width: "100%" }}>
+                <tbody className={style.personalTbody}>
+                  <tr>
+                    <td className={style.personalLabel}>Correo:{"  "}</td>
+                    <td
+                      style={{
+                        fontWeight: "bold",
+                        fontSize: "1.1em",
+                        paddingLeft: "1em",
+                      }}
+                    >
+                      {userInfo.EMAIL.substring(0, 3) +
+                        "****" +
+                        userInfo.EMAIL.substring(userInfo.EMAIL.length - 10)}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className={style.personalLabel}>Contraseña:</td>
+                    <td
+                      style={{
+                        fontWeight: "bold",
+                        fontSize: "1.1em",
+                        paddingLeft: "1em",
+                      }}
+                    >
+                      *********
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </Card>
         </div>
-        <div className={style.rigth}></div>
-        <Grid columns={"equal"} stackable>
-          <Grid.Column width={10}>
-            <Card className={style.card1}>
-              <Card.Content>
-                <Card.Description>
-                  <h1 className={style.h3}>
-                    Experiencia laboral <PlusCircleOutlined />
-                  </h1>
-                  <h3>Aún no has agregado ninguna experiencia laboral</h3>
-                  <p>
-                    Aumenta tus probabilidades de exito agregando experiencia en
-                    este campo
-                  </p>
-                </Card.Description>
-              </Card.Content>
-            </Card>
-            <Card className={style.card1}>
-              <Card.Content>
-                <Card.Description>
-                  <h1 className={style.h3}>
-                    Grado educativo <PlusCircleOutlined />
-                  </h1>
-                  <h3>Aún no has agregado ningun grado educativo</h3>
-                  <p>
-                    Aumenta tus probabilidades de exito agregando experiencia en
-                    este campo
-                  </p>
-                </Card.Description>
-              </Card.Content>
-            </Card>
-            <Card className={style.card}>
-              <Card.Content>
-                <Card.Description>
-                  <h1 className={style.h3}>
-                    Cursos y certificaciones <PlusCircleOutlined />
-                  </h1>
-                  <h3>Aún no has agregado ningun curso o certificación</h3>
-                  <p>
-                    Aumenta tus probabilidades de exito agregando experiencia en
-                    este campo
-                  </p>
-                </Card.Description>
-              </Card.Content>
-            </Card>
-          </Grid.Column>
-        </Grid>
+        <div className={style.rigth}>
+          <Card className={style.card1}>
+            <div className={style.dataCardHeader}>
+              <h1>Experiencia laboral</h1>
+              <button className={style.enableEditButton}>
+                <Icon
+                  name="add circle"
+                  style={{ marginLeft: "0.5em" }}
+                  size="large"
+                />
+              </button>
+            </div>
+            <Card.Content>
+              <Card.Description>
+                <h3>Aún no has agregado ninguna experiencia laboral</h3>
+                <p>
+                  Aumenta tus probabilidades de exito agregando experiencia en
+                  este campo
+                </p>
+              </Card.Description>
+            </Card.Content>
+          </Card>
+          <Card className={style.card1}>
+            <div className={style.dataCardHeader}>
+              <h1>Grado Educativo</h1>
+              <button className={style.enableEditButton}>
+                <Icon
+                  name="add circle"
+                  style={{ marginLeft: "0.5em" }}
+                  size="large"
+                />
+              </button>
+            </div>
+            <Card.Content>
+              <Card.Description>
+                <h3>Aún no has agregado ningun grado educativo</h3>
+                <p>
+                  Aumenta tus probabilidades de exito agregando experiencia en
+                  este campo
+                </p>
+              </Card.Description>
+            </Card.Content>
+          </Card>
+          <Card className={style.card}>
+            <div className={style.dataCardHeader}>
+              <h1>Cursos y certificaciones</h1>
+              <button className={style.enableEditButton}>
+                <Icon
+                  name="add circle"
+                  style={{ marginLeft: "0.5em" }}
+                  size="large"
+                />
+              </button>
+            </div>
+            <Card.Content>
+              <Card.Description>
+                <h3>Aún no has agregado ningun curso o certificación</h3>
+                <p>
+                  Aumenta tus probabilidades de exito agregando experiencia en
+                  este campo
+                </p>
+              </Card.Description>
+            </Card.Content>
+          </Card>
+        </div>
       </div>
       <Footer />
     </>
