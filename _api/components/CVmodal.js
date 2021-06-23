0b7bcacfae95acdaca8c9200a9001cec625fa0fe
qkrooz <46641568+qkrooz@users.noreply.github.com>
2021-06-23 05:36:49 -0600

    .

--- _api/components/CVmodal.js
+++ _api/components/CVmodal.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { PDFDownloadLink } from "@react-pdf/renderer";
+import { PDFDownloadLink, PDFViewer } from "@react-pdf/renderer";
 import CVpdf from "./CVpdf";
 import styled from "styled-components";
 import style from "../../styles/pdfcv.module.css";
@@ -19,164 +19,174 @@ export default function CVmodal() {
       <Modal isOpen={cvmodal_vis} size="5xl" onClose={() => set_cvmodal_vis((cvmodal_vis) => !cvmodal_vis)}>
         <ModalOverlay />
         <ModalContent>
-          <ModalBody padding={0}>
-            <div className={style.container}>
-              <CVButtonsContainer>
-                <CVButtonAction onClick={() => set_cvmodal_vis((cvmodal_vis) => !cvmodal_vis)}>
-                  <Close />
-                </CVButtonAction>
-                <CVButtonAction>
-                  <PDFDownloadLink document={<CVpdf />} fileName={`${userInfo.ID}_CV.pdf`}>
-                    {({ blob, url, loading, error }) => (loading ? "" : <CloudDownloadTwoTone />)}
-                  </PDFDownloadLink>
-                </CVButtonAction>
-              </CVButtonsContainer>
-              <div className={style.header}>
-                <div className={style.logo}>
-                  <h1>Descubre</h1>
-                  <span>El currículum de </span>
-                </div>
-                <div className={style.photo}>
-                  <img src={userInfo.IMAGE_URL} />
-                </div>
-                <div className={style.userPrimaryInfo}>
-                  <span>{`${userInfo.NAMES} ${userInfo.LAST_NAME} ${userInfo.MOTHERS_LAST_NAME} `}</span>
-                  <div style={{ display: "flex", justifyContent: "flex-start" }}>
-                    <span style={{ textTransform: "capitalize", fontWeight: "bold", marginRight: "0.5em" }}>{secondaryInfo.TITULO}</span>
-                    <span>{`${userInfo.GENRE === "masculino" ? "Hombre de" : userInfo.GENRE === "none" ? "" : "Mujer de"} ${userInfo.AGE} años`}</span>
-                  </div>
-                  <div>
-                    <span style={{ fontWeight: "bold" }}>Teléfono</span>
-                    <span>{` ${userInfo.TEL_NUMBER}`}</span>
-                  </div>
-                  <div>
-                    <span style={{ fontWeight: "bold" }}>Correo</span>
-                    <span>{` ${userInfo.EMAIL}`}</span>
-                  </div>
-                  <div>
-                    <span style={{ fontWeight: "bold" }}>Ubicación</span>
-                    <span style={{ textTransform: "capitalize" }}>{` ${userInfo.CITY}, ${userInfo.STATE}`}</span>
-                  </div>
-                </div>
-              </div>
-              <div className={style.body}>
-                <div className={style.bodyFix}>
-                  <div className={style.section}>
-                    <div className={style.sectionHeader}>
-                      {/* <div /> */}
-                      <span>Experiencia laboral</span>
-                    </div>
-                    {JSON.parse(secondaryInfo.EXPERIENCIA_LABORAL).map((key) => (
-                      <div key={key.ID} className={style.sectionItem}>
-                        <div
-                          style={{
-                            display: "flex",
-                            flexDirection: "column",
-                            marginLeft: "5em",
-                            marginBottom: "1em",
-                          }}
-                        >
-                          <div>
-                            <span style={{ fontWeight: "bold", fontSize: "1.1em" }}>{`${key.PUESTO} `}</span>
-                            <span>{!key.STILL ? `de ${key.FECHA_INICIO} a ${key.FECHA_FIN}` : `desde ${key.FECHA_INICIO}`}</span>
-                          </div>
-                          <span>{key.EMPRESA}</span>
-                          <p>{key.DESCRIPCION}</p>
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                  <div className={style.section}>
-                    <div className={style.sectionHeader}>
-                      <span>Grado educativo</span>
-                    </div>
-                    {JSON.parse(secondaryInfo.GRADO_EDUCATIVO).map((key) => (
-                      <div key={key.ID} className={style.sectionItem}>
-                        <div
-                          style={{
-                            display: "flex",
-                            flexDirection: "column",
-                            marginLeft: "5em",
-                            marginBottom: "1em",
-                          }}
-                        >
-                          <div>
-                            <span style={{ fontWeight: "bold", fontSize: "1.1em" }}>{`${key.TITULO_ACADEMICO} `}</span>
-                            <span>{!key.STILL ? `de ${key.FECHA_INICIO} a ${key.FECHA_FIN}` : `desde ${key.FECHA_INICIO}`}</span>
-                          </div>
-                          <span>{key.INSTITUCION}</span>
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                  <div className={style.section}>
-                    <div className={style.sectionHeader}>
-                      <span>Cursos y certificaciones</span>
-                    </div>
-                    {JSON.parse(secondaryInfo.CURSOS_CERTIFICACIONES).map((key) => (
-                      <div key={key.ID} className={style.sectionItem}>
-                        <div
-                          style={{
-                            display: "flex",
-                            flexDirection: "column",
-                            marginLeft: "5em",
-                            marginBottom: "1em",
-                          }}
-                        >
-                          <div>
-                            <span style={{ fontWeight: "bold", fontSize: "1.1em" }}>{`${key.TITULO_CURSO} `}</span>
-                            <span>{` Impartido en ${key.FECHA_INICIO}`}</span>
-                          </div>
-                          <span>{key.TIPO}</span>
-                          <p>{key.DESCRIPTION}</p>
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                  <div className={style.section}>
-                    <div className={style.sectionHeader}>
-                      <span>Habilidades</span>
-                    </div>
-                    <div className={style.sectionItem}>
-                      <div
-                        style={{
-                          marginLeft: "5em",
-                          marginBottom: "1em",
-                        }}
-                      >
-                        <ul>
-                          {JSON.parse(secondaryInfo.HABILIDADES).map((key, i) => (
-                            <li key={i}>{`${key.TITLE}  `}</li>
-                          ))}
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                  <div className={style.section}>
-                    <div className={style.sectionHeader}>
-                      <span>Idiomas</span>
-                    </div>
-                    <div className={style.sectionItem}>
-                      <div
-                        style={{
-                          marginLeft: "5em",
-                          marginBottom: "1em",
-                        }}
-                      >
-                        <ul>
-                          {JSON.parse(secondaryInfo.IDIOMAS).map((key, i) => (
-                            <li key={i}>{`${key.TITLE}  `}</li>
-                          ))}
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <ModalBody>
+            <PDFViewer style={{ height: "50em", width: "100%" }}>
+              <CVpdf userInfo={userInfo} secondaryInfo={secondaryInfo} />
+            </PDFViewer>
           </ModalBody>
         </ModalContent>
       </Modal>
+      // <Modal isOpen={cvmodal_vis} size="5xl" onClose={() => set_cvmodal_vis((cvmodal_vis) => !cvmodal_vis)}>
+      //   <ModalOverlay />
+      //   <ModalContent>
+      //     <ModalBody padding={0}>
+      //       <div className={style.container}>
+      //         <CVButtonsContainer>
+      //           <CVButtonAction onClick={() => set_cvmodal_vis((cvmodal_vis) => !cvmodal_vis)}>
+      //             <Close />
+      //           </CVButtonAction>
+      //           <CVButtonAction>
+      //             <PDFDownloadLink document={<CVpdf userInfo={userInfo} secondaryInfo={secondaryInfo} />} fileName={`${userInfo.ID}_CV.pdf`}>
+      //               {({ blob, url, loading, error }) => (loading ? "" : <CloudDownloadTwoTone />)}
+      //             </PDFDownloadLink>
+      //           </CVButtonAction>
+      //         </CVButtonsContainer>
+      //         <div className={style.header}>
+      //           <div className={style.logo}>
+      //             <h1>Descubre</h1>
+      //             <span>El currículum de </span>
+      //           </div>
+      //           <div className={style.photo}>
+      //             <img src={userInfo.IMAGE_URL} />
+      //           </div>
+      //           <div className={style.userPrimaryInfo}>
+      //             <span>{`${userInfo.NAMES} ${userInfo.LAST_NAME} ${userInfo.MOTHERS_LAST_NAME} `}</span>
+      //             <div style={{ display: "flex", justifyContent: "flex-start" }}>
+      //               <span style={{ textTransform: "capitalize", fontWeight: "bold", marginRight: "0.5em" }}>{secondaryInfo.TITULO}</span>
+      //               <span>{`${userInfo.GENRE === "masculino" ? "Hombre de" : userInfo.GENRE === "none" ? "" : "Mujer de"} ${userInfo.AGE} años`}</span>
+      //             </div>
+      //             <div>
+      //               <span style={{ fontWeight: "bold" }}>Teléfono</span>
+      //               <span>{` ${userInfo.TEL_NUMBER}`}</span>
+      //             </div>
+      //             <div>
+      //               <span style={{ fontWeight: "bold" }}>Correo</span>
+      //               <span>{` ${userInfo.EMAIL}`}</span>
+      //             </div>
+      //             <div>
+      //               <span style={{ fontWeight: "bold" }}>Ubicación</span>
+      //               <span style={{ textTransform: "capitalize" }}>{` ${userInfo.CITY}, ${userInfo.STATE}`}</span>
+      //             </div>
+      //           </div>
+      //         </div>
+      //         <div className={style.body}>
+      //           <div className={style.bodyFix}>
+      //             <div className={style.section}>
+      //               <div className={style.sectionHeader}>
+      //                 {/* <div /> */}
+      //                 <span>Experiencia laboral</span>
+      //               </div>
+      //               {JSON.parse(secondaryInfo.EXPERIENCIA_LABORAL).map((key) => (
+      //                 <div key={key.ID} className={style.sectionItem}>
+      //                   <div
+      //                     style={{
+      //                       display: "flex",
+      //                       flexDirection: "column",
+      //                       marginLeft: "5em",
+      //                       marginBottom: "1em",
+      //                     }}
+      //                   >
+      //                     <div>
+      //                       <span style={{ fontWeight: "bold", fontSize: "1.1em" }}>{`${key.PUESTO} `}</span>
+      //                       <span>{!key.STILL ? `de ${key.FECHA_INICIO} a ${key.FECHA_FIN}` : `desde ${key.FECHA_INICIO}`}</span>
+      //                     </div>
+      //                     <span>{key.EMPRESA}</span>
+      //                     <p>{key.DESCRIPCION}</p>
+      //                   </div>
+      //                 </div>
+      //               ))}
+      //             </div>
+      //             <div className={style.section}>
+      //               <div className={style.sectionHeader}>
+      //                 <span>Grado educativo</span>
+      //               </div>
+      //               {JSON.parse(secondaryInfo.GRADO_EDUCATIVO).map((key) => (
+      //                 <div key={key.ID} className={style.sectionItem}>
+      //                   <div
+      //                     style={{
+      //                       display: "flex",
+      //                       flexDirection: "column",
+      //                       marginLeft: "5em",
+      //                       marginBottom: "1em",
+      //                     }}
+      //                   >
+      //                     <div>
+      //                       <span style={{ fontWeight: "bold", fontSize: "1.1em" }}>{`${key.TITULO_ACADEMICO} `}</span>
+      //                       <span>{!key.STILL ? `de ${key.FECHA_INICIO} a ${key.FECHA_FIN}` : `desde ${key.FECHA_INICIO}`}</span>
+      //                     </div>
+      //                     <span>{key.INSTITUCION}</span>
+      //                   </div>
+      //                 </div>
+      //               ))}
+      //             </div>
+      //             <div className={style.section}>
+      //               <div className={style.sectionHeader}>
+      //                 <span>Cursos y certificaciones</span>
+      //               </div>
+      //               {JSON.parse(secondaryInfo.CURSOS_CERTIFICACIONES).map((key) => (
+      //                 <div key={key.ID} className={style.sectionItem}>
+      //                   <div
+      //                     style={{
+      //                       display: "flex",
+      //                       flexDirection: "column",
+      //                       marginLeft: "5em",
+      //                       marginBottom: "1em",
+      //                     }}
+      //                   >
+      //                     <div>
+      //                       <span style={{ fontWeight: "bold", fontSize: "1.1em" }}>{`${key.TITULO_CURSO} `}</span>
+      //                       <span>{` Impartido en ${key.FECHA_INICIO}`}</span>
+      //                     </div>
+      //                     <span>{key.TIPO}</span>
+      //                     <p>{key.DESCRIPTION}</p>
+      //                   </div>
+      //                 </div>
+      //               ))}
+      //             </div>
+      //             <div className={style.section}>
+      //               <div className={style.sectionHeader}>
+      //                 <span>Habilidades</span>
+      //               </div>
+      //               <div className={style.sectionItem}>
+      //                 <div
+      //                   style={{
+      //                     marginLeft: "5em",
+      //                     marginBottom: "1em",
+      //                   }}
+      //                 >
+      //                   <ul>
+      //                     {JSON.parse(secondaryInfo.HABILIDADES).map((key, i) => (
+      //                       <li key={i}>{`${key.TITLE}  `}</li>
+      //                     ))}
+      //                   </ul>
+      //                 </div>
+      //               </div>
+      //             </div>
+      //             <div className={style.section}>
+      //               <div className={style.sectionHeader}>
+      //                 <span>Idiomas</span>
+      //               </div>
+      //               <div className={style.sectionItem}>
+      //                 <div
+      //                   style={{
+      //                     marginLeft: "5em",
+      //                     marginBottom: "1em",
+      //                   }}
+      //                 >
+      //                   <ul>
+      //                     {JSON.parse(secondaryInfo.IDIOMAS).map((key, i) => (
+      //                       <li key={i}>{`${key.TITLE}  `}</li>
+      //                     ))}
+      //                   </ul>
+      //                 </div>
+      //               </div>
+      //             </div>
+      //           </div>
+      //         </div>
+      //       </div>
+      //     </ModalBody>
+      //   </ModalContent>
+      // </Modal>
     );
   } else {
     return null;
--- _api/components/CVpdf.js
+++ _api/components/CVpdf.js
@@ -1,8 +1,11 @@
 import React from "react";
 import apiRoute from "../resources/apiRoute";
 import { Page, Text, View, Document, StyleSheet, Image } from "@react-pdf/renderer";
-
 const styles = StyleSheet.create({
+  page: {
+    flexDirection: "column",
+    backgroundColor: "#F1F2F4",
+  },
   header: {
     backgroundColor: "#C40033",
     display: "flex",
@@ -13,30 +16,29 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     alignItems: "flex-end",
   },
-  userImageContainer: {},
-  page: {
-    flexDirection: "column",
-    backgroundColor: "#F1F2F4",
-  },
+  userImageContainer: { width: "5em", height: "5em" },
+  userPrimaryInfo: {},
   section: {
     margin: 10,
     padding: 10,
     flexGrow: 1,
   },
 });
+
 const CVpdf = React.memo(({ userInfo, secondaryInfo }) => {
+  console.log(userInfo);
   return (
     <Document>
       <Page size="LETTER" style={styles.page}>
         <View style={styles.header}>
           <View style={styles.logoContainer}>
+            <Image src={DescubreLogo} />
             <Text style={{ fontSize: 10, margin: 0, padding: 0 }}>Descubre</Text>
             <Text style={{ fontSize: 10 }}>El curriculum de</Text>
           </View>
-          <View style={styles.userImageContainer}>
-            {/* <Image
-                src={`${apiRoute}/img/userprofile/${userInfo.IMAGE_URL}`}
-              /> */}
+          <View style={styles.userImageContainer}></View>
+          <View style={styles.userPrimaryInfo}>
+            <Text style={{ textTransform: "capitalize" }}>{`${userInfo.NAMES} ${userInfo.LAST_NAME} ${userInfo.MOTHERS_LAST_NAME}`}</Text>
           </View>
         </View>
         <View style={styles.section}>
